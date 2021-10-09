50c1c83954c2a7657408ebf9fedd67047de08247
Ilya Burenkov <wolferise@gmail.com>
2021-10-09 11:28:53 +0300

    fixed test findind errors

--- test_student.py
+++ test_student.py
@@ -2,10 +2,15 @@ import pytest as pt
 from script_to_test import Student
 
 
-def dict_test():
+def test_dict():
     assert Student("Ilya", "Burenkov", "3").to_dict() == {"first name": "Ilya",
                                                           "second name": "Burenkov",
                                                           "grade": "3"}
 
-if __name__ == "__main__":
-    dict_test()
+
+def test_sample():
+    assert True
+
+
+def test_sample2():
+    assert False
