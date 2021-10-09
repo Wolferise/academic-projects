4fa5bf6c075308703a5398eb3639d7d4df788de4
Ilya Burenkov <wolferise@gmail.com>
2021-10-09 11:48:23 +0300

    added new tests

--- script_to_test.py
+++ script_to_test.py
@@ -1,8 +1,17 @@
 class Student:
     def __init__(self, first_name, second_name, grade):
-        self.first_name = first_name
-        self.second_name = second_name
-        self.grade = grade
+        if first_name == "":
+            self.first_name = "not specified"
+        else:
+            self.first_name = first_name
+        if second_name == "":
+            self.second_name = "not specified"
+        else:
+            self.second_name = second_name
+        if grade == "" or int(grade) > 6:
+            self.grade = "not specified"
+        else:
+            self.grade = grade
 
     def to_dict(self):
         student_dict = {"first name": self.first_name,
--- test_student.py
+++ test_student.py
@@ -2,15 +2,19 @@ import pytest as pt
 from script_to_test import Student
 
 
-def test_dict():
+def test_to_dict():
     assert Student("Ilya", "Burenkov", "3").to_dict() == {"first name": "Ilya",
                                                           "second name": "Burenkov",
                                                           "grade": "3"}
 
 
-def test_sample():
-    assert True
+def test_empty_fields():
+    assert Student("", "", "").to_dict() == {"first name": "not specified",
+                                             "second name": "not specified",
+                                             "grade": "not specified"}
 
 
-def test_sample2():
-    assert False
+def test_inappropriate_grade():
+    assert Student("Ilya", "Burenkov", "7").to_dict() == {"first name": "Ilya",
+                                                          "second name": "Burenkov",
+                                                          "grade": "not specified"}
